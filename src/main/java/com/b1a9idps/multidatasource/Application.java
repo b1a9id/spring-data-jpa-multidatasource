68b72b7113fb9a45455ce3ffafea9877b879e5ec
Ryosuke Uchitate <ruchitate@st.inc>
2023-12-13 20:24:41 +0900

    Initial commit

---
+++ src/main/java/com/b1a9idps/multidatasource/Application.java
@@ -0,0 +1,13 @@
+package com.b1a9idps.springdatajpamultidatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaMultidatasourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaMultidatasourceApplication.class, args);
+	}
+
+}
