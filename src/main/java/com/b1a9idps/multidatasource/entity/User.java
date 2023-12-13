e5c013b5c8f694d99832a7ffdb394fff5b17869e
Ryosuke Uchitate <ruchitate@st.inc>
2023-12-13 20:42:49 +0900

    JPAエンティティ実装

---
+++ src/main/java/com/b1a9idps/multidatasource/entity/User.java
@@ -0,0 +1,41 @@
+package com.b1a9idps.multidatasource.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    private Integer age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
